74cd561b879cc14bf68d9257ce107dcf86b9f1a2
extasy44 <seoheejun@gmail.com>
2020-10-17 14:16:23 +1100

    configuration properties & environmental variables, custom commands, class based properties

--- cypress/integration/examples/Test8Framework.js
+++ cypress/integration/examples/Test8Framework.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+
+//node_modules/.bin/cypress run --spec cypress/integration/examples/Test8Framwork.js --env url=http://google.com --headed
 import HomePage from '../objects/HomePage';
 
 describe('Framework-1', () => {
@@ -11,7 +13,7 @@ describe('Framework-1', () => {
 
   it('Test8, Fixtures and Custom Commands', function () {
     const homePage = new HomePage();
-    cy.visit('https://rahulshettyacademy.com/angularpractice/');
+    cy.visit(Cypress.env('url')); // get value from env variables 'cypress.json'
 
     homePage.getEditBox().type(this.data.name);
     //Two way databinding
@@ -35,6 +37,33 @@ describe('Framework-1', () => {
     //cy.selectProduct('Nokia Edge');
     this.data.productName.forEach((element) => {
       cy.selectProduct(element);
+    });
+
+    cy.get('tr td:nth-child(4) strong')
+      .each(($el, index, $list) => {
+        const amount = $el.text();
+        var res = amount.split(' ');
+        res = res[1].trim();
+        sum = Number(sum) + Number(res);
+      })
+      .then(function () {
+        cy.log(sum);
+      });
+    cy.get('h3 strong').then(function (element) {
+      const amount = element.text();
+      var res = amount.split(' ');
+      var total = res[1].trim();
+      expect(Number(total)).to.equal(sum);
+    });
+    cy.contains('Checkout').click();
+    cy.get('#country').type('India');
+    cy.get('.suggestions > ul > li > a').click();
+    cy.get('#checkbox2').click({ force: true });
+    cy.get('input[type="submit"]').click();
+    //cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :-).')
+    cy.get('.alert').then(function (element) {
+      const actualText = element.text();
+      expect(actualText.includes('Success')).to.be.true;
     });
   });
 });
